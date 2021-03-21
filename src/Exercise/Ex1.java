2d832101ff5bedda1aa98a5816a6b4eaf3f9d675
ArtyomPomin <artyom.pomin@yandex.ru>
2021-03-22 00:36:51 +0300

    Initial commit

---
+++ src/Exercise/Ex1.java
@@ -0,0 +1,10 @@
+package Exercise;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        for (int count = 1; count <= 100; count++) {
+            System.out.println("Hello World!" + count);
+        }
+    }
+}
---
+++ src/Exercise/Ex2.java
@@ -0,0 +1,24 @@
+package Exercise;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner num = new Scanner (System.in);
+        System.out.println("Введите число");
+        int k;
+        k = num.nextInt();
+
+        switch (k) {
+            case 1 : System.out.print("пн"); break;
+            case 2 : System.out.print("вт"); break;
+            case 3 : System.out.print("ср"); break;
+            case 4 : System.out.print("чт"); break;
+            case 5 : System.out.print("пт"); break;
+            case 6 : System.out.print("сб"); break;
+            case 7 : System.out.print("вс"); break;
+            default : System.out.print("Error"); break;
+        }
+    }
+}
