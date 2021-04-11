3e050b3e023346024821a662569f37952f7cd43b
ArtyomPomin <artyom.pomin@yandex.ru>
2021-04-11 21:50:36 +0300

    change

--- src/Exercise/Ex4.java
+++ src/Exercise/Ex4.java
@@ -6,27 +6,33 @@ public class Ex4 {
     public static void main(String[] args) {
         Scanner num1 = new Scanner(System.in);
         System.out.println("Введите число a ");
-        double a;
-        a = num1.nextInt();
+        double a = num1.nextInt();
 
         Scanner num2 = new Scanner(System.in);
         System.out.println("Введите число b ");
-        double b;
-        b = num2.nextInt();
+        double b = num2.nextInt();
 
         Scanner num3 = new Scanner(System.in);
         System.out.println("Введите число c ");
-        double c;
-        c = num3.nextInt();
+        double c = num3.nextInt();
 
-        double D = pow(b,2) - 4 * a * c;
+        double D = pow(b, 2) - 4 * a * c;
 
-        double x1 = (-b + sqrt(D)) / 2*a;
-        System.out.println("Первый корень X = " + x1);
+        if (D > 0) {
+            double x1 = (-b + sqrt(D)) / 2 * a;
+            System.out.println("Первый корень X = " + ((int)x1));
 
-        double x2 = (-b - sqrt(D)) / 2*a;
-        System.out.println("Второй корень X = " + x2);
+            double x2 = (-b - sqrt(D)) / 2 * a;
+            System.out.println("Второй корень X = " + ((int)x2));
+        } else if (D == 0) {
+            double x = -b / (2 * a);
+            System.out.println("Корень X = " + ((int)x));
+        } else
+            System.out.println("Уравнение не имеет корней");
 
     }
-
 }
+
+
+
+
