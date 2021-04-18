1ff0f9555a9f79283173069416ac5b546fba0373
ArtyomPomin <artyom.pomin@yandex.ru>
2021-04-18 22:35:02 +0300

    change

--- src/Exercise/Ex15.java
+++ src/Exercise/Ex15.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Ex15 {
     public static void main(String[] args) {
-        int F = 0;
+        int F;
         Scanner num = new Scanner(System.in);
         System.out.println("Введите число: ");
 
@@ -13,17 +13,19 @@ public class Ex15 {
             F = num.nextInt();
         } catch (Exception e) {
             System.out.println("Ошибка: введён неверный символ,");
+            return;
         }
-       if (F > 0) {
+        if (F > 0) {
             int x = 1;
             for (int i = 1; i <= F; i++) {
                 x = i * x;
-                System.out.println("факториал " + i + " равен: " +x);
             }
+            System.out.println("факториал " + F + " равен: " + x);
         } else {
-            System.out.println("отрицательное число");
+            System.out.println("Ошибка: введите положительное число");
         }
     }
 }
+
 
 
