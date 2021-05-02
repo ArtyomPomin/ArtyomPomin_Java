e3b5fd965fb72d7c48f48734a022098be89aa95b
ArtyomPomin <artyom.pomin@yandex.ru>
2021-05-02 23:18:49 +0300

    change

---
+++ src/Exercise/Ex16.java
@@ -0,0 +1,22 @@
+package Exercise;
+import java.util.Scanner;
+
+//Анализ креативности текста
+
+public class Ex16 {
+    public static void main(String[] args) {
+        String s;
+        int count = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        s = input.nextLine();
+
+        for (int i = 0; i < s.length(); i++) {
+           if (s.charAt(i) !=',' & s.charAt(i) !='.' & s.charAt(i) !='!' & s.charAt(i) !='?' & s.charAt(i) !=':' & s.charAt(i) !=';' & s.charAt(i) !=' ' & s.charAt(i) !='(' & s.charAt(i) !=')') {
+            count++;
+            }
+        }
+        System.out.println("количество букв в тексте: " +count);
+    }
+}
