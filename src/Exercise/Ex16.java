3ddc024e8ac0dfe93c1eb8febe5f6c906336044c
ArtyomPomin <artyom.pomin@yandex.ru>
2021-05-04 17:09:34 +0300

    change

--- src/Exercise/Ex16.java
+++ src/Exercise/Ex16.java
@@ -7,17 +7,43 @@ import java.util.Scanner;
 public class Ex16 {
     public static void main(String[] args) {
         String s;
-        int count = 0;
+        int numLetter = 0;
+        int numWords = 0;
+        boolean foundDiv = true;
+        boolean foundChar;
 
         Scanner input = new Scanner(System.in);
         System.out.println("Введите текст: ");
         s = input.nextLine();
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != ',' && s.charAt(i) != '.' && s.charAt(i) != '!' && s.charAt(i) != '?' && s.charAt(i) != ':' && s.charAt(i) != ';' && s.charAt(i) != ' ' && s.charAt(i) != '(' && s.charAt(i) != ')') {
-                count++;
+            if (s.charAt(i) != ',' &&
+                    s.charAt(i) != '.' &&
+                    s.charAt(i) != '!' &&
+                    s.charAt(i) != '?' &&
+                    s.charAt(i) != ':' &&
+                    s.charAt(i) != ';' &&
+                    s.charAt(i) != ' ' &&
+                    s.charAt(i) != '(' &&
+                    s.charAt(i) != ')') {
+                numLetter++;
             }
         }
-        System.out.println("количество букв в тексте: " + count);
+        System.out.println("количество букв в тексте: " + numLetter);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ' || s.charAt(i) == '.') {
+                foundDiv = true;
+                foundChar = false;
+            } else {
+                foundChar = true;
+            }
+            if (foundChar && foundDiv) {
+                numWords++;
+                foundDiv = false;
+            }
+        }
+        System.out.println("количество слов в тексте: " + numWords);
     }
 }
+
